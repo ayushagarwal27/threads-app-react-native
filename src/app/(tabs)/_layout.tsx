a56f4517db7bf240032d7ea902fb9e419e4eda08
ayush <ayush@ayushs-MacBook-Air-2.local>
2025-05-18 19:02:45 +0530

    add theme

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ export default function TabsLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "black",
+        // tabBarActiveTintColor: "black",
         tabBarShowLabel: false,
       }}
     >
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { Slot } from "expo-router";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+
+const myTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    primary: "white",
+  },
+};
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <ThemeProvider value={myTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
 }
