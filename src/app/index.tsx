b6e1e54641db041c23272fb667f502bdc54721f6
ayush <ayush@ayushs-MacBook-Air-2.local>
2025-05-18 17:15:49 +0530

    add expo-router and nativewind integration

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+// import "./global.css";
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
