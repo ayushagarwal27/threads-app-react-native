e5ea90c19347ff1e90aea333d0fbedc1bbd0f223
ayush <ayush@ayushs-MacBook-Air-2.local>
2025-05-18 18:55:20 +0530

    add expo botoom tabs

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
-// import "./global.css";
 
-export default function App() {
+export default function ProfileScreen() {
   return (
     <View>
-      <Text>App</Text>
+      <Text>ProfileScreen</Text>
     </View>
   );
 }
